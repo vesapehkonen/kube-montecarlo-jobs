fd43b9555dabe238168910eef9c7e09dbae23bca
Vesa Pehonen <vesa.pehkonen@gmail.com>
2026-01-09 14:29:29 -0800

    Chaged to OS to Ubuntu.

--- infra/main.tf
+++ infra/main.tf
@@ -115,11 +115,10 @@ data "aws_ami" "ubuntu_2204" {
 }
 
 resource "aws_instance" "this" {
-  ami                    = data.aws_ami.al2023.id
+  ami                    = data.aws_ami.ubuntu_2204.id
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web.id]
-
   associate_public_ip_address = true
 
   tags = { Name = "${local.name}-ec2" }
