eaa09333de3d86694ffe45d5d8e7bfe3790b9de1
Vesa Pehonen <vesa.pehkonen@gmail.com>
2026-01-09 14:25:22 -0800

    Chaged to OS to Ubunut.

--- infra/main.tf
+++ infra/main.tf
@@ -98,14 +98,19 @@ resource "aws_security_group" "web" {
   tags = { Name = "${local.name}-sg" }
 }
 
-# Latest Amazon Linux 2023
-data "aws_ami" "al2023" {
+# Latest Ubuntu 22.04 LTS (Jammy) AMI from Canonical
+data "aws_ami" "ubuntu_2204" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
